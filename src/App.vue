bda3e17f603e09c8bcd278594c80c7078822959e
Alan Strassmeier <102873586+alan-strassmeier@users.noreply.github.com>
2024-05-20 21:02:35 +0200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1> URL Shortener</h1>
+    <Title />
     <InputForm />
     <BgStyle />
   </div>
@@ -9,17 +9,5 @@
 <script setup>
 import BgStyle from "./components/BgStyle.vue"
 import InputForm from "./components/InputForm.vue"
+import Title from "./components/Title.vue"
 </script>
-
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Libre+Barcode+39+Extended+Text&display=swap');
-
-h1 {
-  font-family: "Libre Barcode 39 Extended Text", system-ui;
-  font-size: 96px;
-  font-weight: 400;
-  font-style: normal;
-  text-align: center;
-  margin-top: 35px;
-}
-</style>
